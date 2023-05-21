32e0c70dc9cc3979359a7c02a06ca0c2bde4566d
Oli Legg <oliglegg@hotmail.co.uk>
2023-05-21 10:19:09 +0100

    Changing speech marks to bar (|)

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import sys
 import re
 
-def remove_quotes(string: str):
-    pattern = r'^"(.*)"$'
-    replacement = r'\1'
-    result = re.sub(pattern, replacement, string)
-    return result
+remove_quotes = lambda string : re.sub(r'^"(.*)"$', r'\1', string)
 
 def change_quote_char(input_file: str, output_file: str):
     with open(input_file, 'r') as inp, open(output_file, 'w', newline='') as out:
         for line in inp:
             line = line.strip()
             fields = line.split(',')
-            fields = ['"' + remove_quotes(field).replace('"', '""') + '"' for field in fields]
-            out.write('|'.join(fields) + '\n')
+            fields = [f"|{remove_quotes(field)}|" for field in fields]
+            out.write(','.join(fields) + '\n')
 
 if __name__ == '__main__':
     input_file = sys.argv[1] or 'input.csv'
